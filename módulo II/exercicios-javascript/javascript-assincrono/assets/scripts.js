d1786301d26cdc03e06bb92a42d53328e133d388
Ivana Feitosa <ivsfeitosa@gmail.com>
2022-08-16 11:25:39 -0300

    incluindo solucao 2 e ajustes

--- módulo II/exercicios-javascript/javascript-assincrono/assets/scripts.js
+++ módulo II/exercicios-javascript/javascript-assincrono/assets/scripts.js
@@ -23,3 +23,27 @@ dogBtn.addEventListener('click', loadImg);
 
 loadImg();
 
+//Solução sem try...catch
+/*
+const BASE_URL = 'https://dog.ceo/api/breeds/image/random';
+
+const dogBtn = document.getElementById('change-dog');
+
+
+const getDogs = async () => {
+    const data = await fetch(BASE_URL)
+        .then((resp) => resp.json())
+        .catch((e) => console.log(e));
+
+    return data.message;
+};
+
+const loadImg = async () => {
+    const dogImg = document.getElementById('dog');
+    dogImg.src = await getDogs();
+};
+
+dogBtn.addEventListener('click', loadImg);
+
+loadImg();
+*/
