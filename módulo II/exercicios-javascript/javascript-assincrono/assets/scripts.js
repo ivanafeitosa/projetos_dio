22594e1bfb5407d0ffa6101131443d75ed42c182
Ivana Feitosa <ivsfeitosa@gmail.com>
2022-08-16 10:18:01 -0300

    page just dogs

---
+++ módulo II/exercicios-javascript/javascript-assincrono/assets/scripts.js
@@ -0,0 +1,25 @@
+const BASE_URL = 'https://dog.ceo/api/breeds/image/random';
+
+const dogBtn = document.getElementById('change-dog');
+
+
+async function getDogs() {
+    try {
+        const data = await fetch(BASE_URL);
+        const json = await data.json();
+
+        return json.message;
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+
+const loadImg = async () => {
+    const dogImg = document.getElementById('dog');
+    dogImg.src = await getDogs();
+};
+
+dogBtn.addEventListener('click', loadImg);
+
+loadImg();
+
