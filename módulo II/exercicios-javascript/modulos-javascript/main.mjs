c4e2007ade58bbb2cd0bfbaeafb478f00dc7d04c
Ivana Feitosa <ivsfeitosa@gmail.com>
2022-08-18 20:45:57 -0300

    aprendendo modulos em javascript

---
+++ módulo II/exercicios-javascript/modulos-javascript/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Ivana", 25));
+console.log(mostraCidade("Ivana", "Fortaleza"));
+console.log(mostraHobby("Ivana", "assistir serie"));
